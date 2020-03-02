9e3bebb1f98fa1ed7e60a9874c9d5c47d21d52f5
igor.krosh <igor.krosh@gmail.com>
2020-03-02 14:15:29 +0300

    Add new plugins (VueX, FontAwesome, Buefy)

---
+++ src/components/TodayPanel.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <b-button type="is-primary">Primary</b-button>
+        <font-awesome-icon icon="spinner" />
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: 'TodayPanel'
+}
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
+import {store} from './store';
+
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+library.add(fas)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.use(Buefy, {
+  defaultIconComponent: 'font-awesome-icon',
+  defaultIconPack: 'fas',
+})
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  store
 }).$mount('#app')
